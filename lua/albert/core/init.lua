99bc8260a311db9d1d242f94dcd909cfeb4b0cea
albrthuynh <albrthuynh@gmail.com>
2026-05-07 21:17:06 -0500

    muted noisy deprecation call

--- lua/albert/core/init.lua
+++ lua/albert/core/init.lua
@@ -1,2 +1,8 @@
+if vim.lsp and vim.lsp.get_clients then
+    vim.lsp.get_active_clients = function(opts)
+        return vim.lsp.get_clients(opts)
+    end
+end
+
 require("albert.core.options")
 require("albert.core.keymaps")
