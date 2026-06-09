154d2e3065bbdb6a988bde47c96d607a0d60ddb8
albrthuynh <ahuyn2@uic.edu>
2026-06-08 19:25:58 -0500

    hopefully figured out all the updates to this config

--- lua/albert/plugins/treesitter.lua
+++ lua/albert/plugins/treesitter.lua
@@ -1,60 +1,41 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
+    branch = "main",
     build = ":TSUpdate",
     dependencies = {
         "windwp/nvim-ts-autotag",
     },
     config = function()
-        -- import nvim-treesitter plugin
-        local treesitter = require("nvim-treesitter.configs")
+        require("nvim-treesitter").setup {
+            -- install_dir = vim.fn.stdpath("data") .. "/site",
+        }
 
-        -- configure treesitter
-        treesitter.setup({ -- enable syntax highlighting
-            highlight = {
-                enable = true,
-            },
-            -- use runtime Lua indentation to avoid TS indent quirks on <CR>
-            indent = {
-                enable = true,
-                disable = { "lua" },
-            },
-
-            -- ensure these language parsers are installed
-            ensure_installed = {
-                "json",
-                "javascript",
-                "typescript",
-                "tsx",
-                "yaml",
-                "html",
-                "css",
-                "prisma",
-                "markdown",
-                "markdown_inline",
-                "svelte",
-                "graphql",
-                "bash",
-                "lua",
-                "vim",
-                "dockerfile",
-                "gitignore",
-                "query",
-                "vimdoc",
-                "c",
-                "cpp",
-                "java",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<c-space>",
-                    node_incremental = "<c-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
+        -- enable treesitter highlighting (provided by Neovim core)
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
         })
-        require("nvim-ts-autotag").setup() -- seperate setup for nvim-ts-autotag
+
+        -- enable treesitter indentation (experimental, from nvim-treesitter)
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                if vim.bo.filetype ~= "lua" then
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end,
+        })
+
+        -- ensure parsers are installed (async)
+        require("nvim-treesitter").install {
+            "json", "javascript", "typescript", "tsx", "yaml", "html", "css",
+            "prisma", "markdown", "markdown_inline", "svelte", "graphql",
+            "bash", "lua", "vim", "dockerfile", "gitignore", "query",
+            "vimdoc", "c", "cpp", "java",
+        }
+
+        require("nvim-ts-autotag").setup()
     end,
 }
